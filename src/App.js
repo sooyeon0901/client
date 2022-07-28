d54fe53a8fe0187b021d503b1937c94bdab43799
suyeon.kim <suyeoun.kim@e4net.net>
2022-07-28 10:26:23 +0900

    서버렌더링

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [ todoList, setTodoList ] = useState(null);
+
+  // 첫 렌더링시에만 데이터를 한번만 가져오도록 
+  useEffect(() => {
+    // 1. fetch 사용
+    fetch('http://localhost:4000/api/todo')
+    .then((res) => res.json()) // 데이터 한번 정제해 줘야 함
+    .then((data) => {
+      //console.log(todoList);
+      setTodoList(data)
+      //console.log('후==', todoList);
+    });
+
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>todo list</div>
+      {todoList && todoList.map((todo) => (
+        <div key={todo.id}>
+          <div>{todo.id}</div>
+          <div>{todo.text}</div>
+          <div>{todo.done ? 'true': 'false'}</div>
+        </div>
+      ))}
     </div>
   );
 }
